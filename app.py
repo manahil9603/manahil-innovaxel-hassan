44e46149ac73edf7c18a8010a1bf938c50be079e
Manahil Hassan <manahilhassan9603@gmail.com>
2025-03-23 15:21:16 +0500

    Serve frontend from Flask

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask,send_from_directory
 from flask_restful import Api
 from flask_cors import CORS
 from models import db
 from resources import ShortenURL,RetrieveURL,UpdateURL,DeleteURL,URLStats
 import logging
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 
 logging.basicConfig(level=logging.DEBUG)
@@ -12,6 +14,23 @@ logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 CORS(app)
+
+
+@app.route('/')
+def serve_frontend():
+    return send_from_directory('../frontend', 'index.html')
+
+
+@app.route('/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('../frontend', filename)
+
+
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    default_limits=["200 per day", "50 per hour"]
+)
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///urls.db'
